d4dbc8cc28ed317798cafa172c48dd084b1909aa
Nikhil Bharadwaj <44049892+pixelbrahma@users.noreply.github.com>
2019-05-03 14:28:12 +0530

    adds

---
+++ Raft Wars/Raft Wars/Assets/Scripts/GunController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+    [SerializeField] private GameObject trajectoryPrefab;
+    [SerializeField] private GameObject shotPrefab;
+    public static float power;
+    public Vector2 direction;
+    const int numberOfProjectiles = 7;
+    struct Position
+    {
+       public float x;
+       public float y;
+    }
+    bool first = true;
+    GameObject[] traj = new GameObject[numberOfProjectiles];
+    bool fire = false;
+
+    void Start () {
+
+	}
+	
+
+	void Update () {
+
+        Vector3 mPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(mPosition);
+        direction = mPosition - transform.position;
+        transform.up = direction.normalized;
+        power = Mathf.Sqrt(direction.x * direction.x + direction.y * direction.y);
+        if (fire == false)
+        {
+            CreateProjectiles();
+            if (Input.GetMouseButtonDown(0))
+            {
+                GameObject shot = Instantiate(shotPrefab, traj[1].transform.position, Quaternion.identity);
+                fire = true;
+            }
+        }
+    }
+
+    void CreateProjectiles()
+    {
+        Vector2[] pos = new Vector2[numberOfProjectiles];
+        float projectileOffsetX = direction.x / numberOfProjectiles;
+        float projectileOffsetY = direction.y / numberOfProjectiles;
+        Position[] trajectoryPoints = new Position[numberOfProjectiles];
+        for (int i = 0; i < numberOfProjectiles; i++)
+        {
+            trajectoryPoints[i].x = transform.position.x + i * projectileOffsetX;
+            trajectoryPoints[i].y = transform.position.y + i * projectileOffsetY;
+            pos[i] = new Vector2(trajectoryPoints[i].x, trajectoryPoints[i].y);
+            if (first)
+            {
+                traj[i] = Instantiate(trajectoryPrefab, pos[i], Quaternion.identity) as GameObject;
+                if (i == numberOfProjectiles - 1)
+                {
+                    first = false;
+                }
+            }
+            traj[i].transform.position = pos[i];
+        }
+    }
+}
---
+++ Raft Wars/Raft Wars/Assets/Scripts/ShotController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotController : MonoBehaviour {
+    //GunController gunScript;
+    GameObject shot;
+    GameObject gun;
+    private Rigidbody2D rb;
+    [SerializeField] private float powerFactor;
+    GameObject main;
+    [SerializeField] private float offset;
+
+    void Start () {
+        shot = GameObject.Find("Shot");
+        gun = GameObject.Find("Gun");
+        main = GameObject.Find("Main Camera");
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = gun.transform.up * GunController.power * powerFactor; 
+	}
+	
+	void Update () {
+        main.transform.position = new Vector3(transform.position.x - offset, transform.position.y, transform.position.z - 10);
+    }
+}
